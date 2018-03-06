1699c96ad4d8451e904140f4ac7180422534a406
thepunisher <khuranalokesh07@gmail.com>
2018-03-06 14:45:46 +0530

    resetting

--- Client/Tut 12 - Client/PacketType.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-enum class PacketType
-{
-	ChatMessage,
-	FileTransferRequestFile, //Sent to request a file
-	FileTransfer_EndOfFile, //Sent for when file transfer is complete
-	FileTransferByteBuffer, //Sent before sending a byte buffer for file transfer
-	FileTransferRequestNextBuffer //Sent to request the next buffer for file
-};
